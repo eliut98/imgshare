9c92a3537f490b3a0d8c63b5bef366784482ff02
roberth_18 <roberthgon18@gmail.com>
2018-12-16 20:11:08 +0100

    Saving images in Database

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const fs = require('fs-extra');
-const { randomName } = require('../helpers/libs');
+const {
+  Image
+} = require('../models/Index');
+const {
+  randomName
+} = require('../helpers/libs');
 
 const ctrl = {};
 
@@ -8,30 +13,50 @@ ctrl.index = (req, res) => {
 
 }
 
-ctrl.create = async (req, res) => {
-    const imgTempPath = req.file.path;
-    const ext = path.extname(req.file.originalname).toLocaleLowerCase();
+ctrl.create = (req, res) => {
+  const saveImage = async () => {
     const imgName = randomName();
-    const targetPath = path.resolve(`src/public/upload/${imgName}${ext}`);
-
-    
-    if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif' || ext === '.webp') {
-      await fs.rename(imgTempPath, targetPath);
+    const images = await Image.find({
+      filename: imgName
+    });
+    if (images.length > 0) {
+      saveImage();
+    } else {
+      const imgTempPath = req.file.path;
+      const ext = path.extname(req.file.originalname).toLocaleLowerCase();
+      const targetPath = path.resolve(`src/public/upload/${imgName}${ext}`);
+      if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif' || ext === '.webp') {
+        await fs.rename(imgTempPath, targetPath);
+        const newImage = new Image({
+          title: req.body.title,
+          description: req.body.description,
+          filename: imgName + ext
+        });
+        const imageSaved = await newImage.save();
+        // res.redirect('/images');
+        res.send('Works');
+      } else {
+        await fs.unlink(imgTempPath);
+        res.status(500).json({
+          msg: 'Only images format are allowed'
+        });
+      }
     }
 
-    res.send('Works');
+  }
+  saveImage();
 }
 
 ctrl.like = (req, res) => {
-    
+
 }
 
 ctrl.comment = (req, res) => {
-    
+
 }
 
 ctrl.remove = (req, res) => {
-    
+
 }
 
 module.exports = ctrl;
